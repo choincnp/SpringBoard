27bb7f59358634f587ab05711ceae25652fcbb90
choincnp <whdudwns7777@gmail.com>
2023-02-07 00:14:02 +0900

    Feat: Initial commits with MainController with finding

---
+++ springboard/src/main/java/com/hanghae/springboard/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.hanghae.springboard.controller;
+
+import com.hanghae.springboard.dto.LetterResponseDto;
+import com.hanghae.springboard.dto.LetterRequestDto;
+import com.hanghae.springboard.entity.Letter;
+import com.hanghae.springboard.service.LetterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class MainController {
+
+    private final LetterService letterService;
+
+    @GetMapping("/posts")
+    public List<Letter> viewAll(){
+        return letterService.findAll();
+    }
+
+
+}
