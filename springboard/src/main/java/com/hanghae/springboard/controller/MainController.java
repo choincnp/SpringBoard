41e83dc2e3101680ad4fea2d61318e1564673d90
choincnp <whdudwns7777@gmail.com>
2023-02-07 02:32:07 +0900

    feat: Add modify, delete method in controller

--- springboard/src/main/java/com/hanghae/springboard/controller/MainController.java
+++ springboard/src/main/java/com/hanghae/springboard/controller/MainController.java
@@ -2,7 +2,6 @@ package com.hanghae.springboard.controller;
 
 import com.hanghae.springboard.dto.LetterResponseDto;
 import com.hanghae.springboard.dto.LetterRequestDto;
-import com.hanghae.springboard.entity.Letter;
 import com.hanghae.springboard.service.LetterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +28,15 @@ public class MainController {
     @GetMapping("/post/{id}")
     public Long viewOne(@PathVariable Long id){
         return letterService.findOne(id).getId();
+    }
+
+    @PutMapping("/post/{id}") //PUT METHOD이기 때문에 모든 내용이 다 들어가야 한다.
+    public Long modifyLetter(@PathVariable Long id, LetterRequestDto letterRequestDto){
+        return letterService.modifyLetter(id,letterRequestDto);
+    }
+    @DeleteMapping("/post/{id}")
+    public Long deleteLetter(@PathVariable Long id, String password){
+        return letterService.deleteLetter(id, password);
     }
 
 
