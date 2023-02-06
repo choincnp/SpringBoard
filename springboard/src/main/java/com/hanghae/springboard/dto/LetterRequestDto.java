4e74ef23ed9fb92eeea9b14952f1ee66ef81c716
choincnp <whdudwns7777@gmail.com>
2023-02-07 00:13:07 +0900

    Feat: Initial commits with letterRequestDto

---
+++ springboard/src/main/java/com/hanghae/springboard/dto/LetterRequestDto.java
@@ -0,0 +1,11 @@
+package com.hanghae.springboard.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LetterRequestDto {
+    private Long id;
+    private String username;
+    private String contents;
+    private String password;
+}
