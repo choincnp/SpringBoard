c8c5ba6acc1588b547a37b304eee8e7ac57d2b46
choincnp <whdudwns7777@gmail.com>
2023-02-06 20:55:12 +0900

    Feat: Add LetterDto Initial featured created, modified time with extending timestamp class

---
+++ springboard/src/main/java/com/hanghae/springboard/entity/Letter.java
@@ -0,0 +1,32 @@
+package com.hanghae.springboard.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Letter extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String contents;
+
+    private String password;
+
+    public Letter(Long id, String username, String contents, String password) {
+        this.id = id;
+        this.username = username;
+        this.contents = contents;
+        this.password = password;
+    }
+
+}
