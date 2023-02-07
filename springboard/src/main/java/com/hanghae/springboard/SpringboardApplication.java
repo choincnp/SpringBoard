72e98feb6e7ae9fe933e229fb04826d819e5f749
choincnp <whdudwns7777@gmail.com>
2023-02-07 10:35:23 +0900

    feat: Add build files

---
+++ springboard/src/main/java/com/hanghae/springboard/SpringboardApplication.java
@@ -0,0 +1,15 @@
+package com.hanghae.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboardApplication.class, args);
+    }
+
+}
