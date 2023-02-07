3dbfcf0abae27c36d42dd784ca90137f2b1d280d
choincnp <whdudwns7777@gmail.com>
2023-02-07 12:44:45 +0900

    feat: Add two more methods case for delete method

--- springboard/src/main/java/com/hanghae/springboard/controller/MainController.java
+++ springboard/src/main/java/com/hanghae/springboard/controller/MainController.java
@@ -5,6 +5,7 @@ import com.hanghae.springboard.dto.LetterRequestDto;
 import com.hanghae.springboard.service.LetterService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,10 +36,20 @@ public class MainController {
     public Long modifyLetter(@PathVariable Long id, @RequestBody LetterRequestDto letterRequestDto){
         return letterService.modifyLetter(id,letterRequestDto);
     }
-    @DeleteMapping("/post/{id}")
+//    @DeleteMapping("/post/{id}") //ModelAttribute 방식으로 구현, body : x-www.form-urlencoded
+//    public String deleteLetter(@PathVariable Long id, @ModelAttribute("password") String password) throws JSONException {
+//        return letterService.deleteLetter(id, password);
+//    }
+
+//    @DeleteMapping("/post/{id}") //REQUESTPARAM 방식으로 구현, body : form-data
+//    public String deleteLetter(@PathVariable Long id, @RequestParam String password, Model m) throws JSONException {
+//        m.addAttribute("password",password);
+//        return letterService.deleteLetter(id, password);
+//    }
+
+    @DeleteMapping("/post/{id}") //RequestBody 방식으로 구현, body : raw - text
     public String deleteLetter(@PathVariable Long id, @RequestBody String password) throws JSONException {
         return letterService.deleteLetter(id, password);
     }
-
 
 }
