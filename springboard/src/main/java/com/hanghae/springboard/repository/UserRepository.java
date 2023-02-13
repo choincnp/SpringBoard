2040f2f9fcd88f50867e554032eb30e91ae5369d
choincnp <whdudwns7777@gmail.com>
2023-02-13 14:47:30 +0900

    feat: Add UserRepository

---
+++ springboard/src/main/java/com/hanghae/springboard/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hanghae.springboard.repository;
+
+import com.hanghae.springboard.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
